696cb83c6f137ea7e7519af85e396ec1cd4bf6d5
vistacyy <vistacyy@163.com>
2017-10-10 22:03:55 +0800

    test

--- public/index.php
+++ public/index.php
@@ -16,40 +16,9 @@
 // // 加载框架引导文件
 // require __DIR__ . '/../thinkphp/start.php';
 
+// echo $_SERVER['SERVER_ADDR'] . ' ' . $_SERVER['SERVER_NAME'];
 
 
-class Connection 
-{
-    protected $link;
-    private $server, $username, $password, $db;
-    
-    public function __construct($server, $username, $password, $db)
-    {
-        $this->server = $server;
-        $this->username = $username;
-        $this->password = $password;
-        $this->db = $db;
-        $this->connect();
-    }
-    
-    private function connect()
-    {
-        $this->link = mysql_connect($this->server, $this->username, $this->password);
-        mysql_select_db($this->db, $this->link);
-    }
-    
-    public function __sleep()
-    {
-        return array('server', 'username', 'password', 'db');
-    }
-    
-    public function __wakeup()
-    {
-        $this->connect();
-    }
-}
-
-
-echo serialize(Connection);
-
-
+$array=[1,2,3];
+// print_r( array_values($array) );
+echo count($array);
---
+++ test.js
@@ -0,0 +1,36 @@
+// 去掉提示 
+function openTishi(second) {
+    if (second == randomTime) //随机时间提示
+    {
+        setRandomTipTime();
+        updateStudyTime(0);
+    }
+}
+
+// 提示保存时间
+function DoConverseCallTimer() {
+    if (isClose == 1) {
+        return;
+    }
+    if (document.form2.thzt.value == "") {
+        //alert("请输入倒计时开始的秒数！");
+    } else {
+        var minute = "0";
+        var second = "0";
+        if (videoifplayFlag) {
+            begintime = parseInt(begintime) + 1; 
+        }
+        minute = parseInt(begintime / 60);
+        second = begintime % 60;
+        if (minute < "21") {
+            document.form2.thzt.value = minute + "分" + second + "秒";
+            jQuery("#benci").html(minute + "分" + second + "秒" + " -" + (randomTime - begintime));
+        } else {
+            document.form2.thzt.value = minute + "分" + second + "秒";
+            return;
+        }
+        timer1 = window.setTimeout("DoConverseCallTimer()", 1000);
+        document.form2.passedtime.value = begintime;
+        openTishi(begintime);
+    }
+}
